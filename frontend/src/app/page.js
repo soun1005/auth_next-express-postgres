d1cbcadd11c69a65f3136af1aff961eb7c36a815
Soeun Lee <thdms1005@gmail.com>
2024-01-24 20:05:41 +0100

    :construction: Add(form validation) : add validation with yup
    react-hook-form and yup are used

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,23 +1,76 @@
+'use client';
+
 import styles from './page.module.css';
 import ButtonWithLink from './components/ButtonWithLink';
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { string } from 'yup';
+import { useRouter } from 'next/navigation';
+
+const schema = yup
+  .object({
+    email: string().required('Required').email('Cet email nest pas valide'),
+    password: string().required('Required'),
+  })
+  .required();
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const router = useRouter();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    router.push('/articles');
+  };
+
   return (
     <main className="formMain">
       <div className="formContainer">
         <h1>Log-in</h1>
         <div className="formDiv">
-          <form className="form">
-            <label for="email">Email</label>
-            <input type="text" name="email" value="" />
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label>Email</label>
+              <input
+                {...register('email')}
+                placeholder="E-mail"
+                required
+                type="text"
+                name="email"
+              />
+            </div>
+            <div className="error">
+              {formState.errors.email?.message !== undefined
+                ? `${formState.errors.email?.message}`
+                : ''}
+            </div>
 
-            <label for="password">Password</label>
-            <input type="password" name="password" value="" />
-          </form>
-          <div className={styles.buttonWrap}>
+            <div>
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                {...register('password')}
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="error">
+              {formState.errors.password?.message !== undefined
+                ? `${formState.errors.password?.message}`
+                : ''}
+            </div>
+
             <button className="loginBtn" type="submit">
               Log in
             </button>
+          </form>
+          <div className={styles.buttonWrap}>
             <ButtonWithLink
               text="Sign up"
               page="signup"
--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -1,29 +1,135 @@
-import styles from './page.module.css';
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { string } from 'yup';
+import { useRouter } from 'next/navigation';
+
+const nameRules = /^[A-Za-z\s]{1,20}$/;
+// alphabets, max 20, white space allowed
+const passwordRules = /^(?=.*[A-Za-z]).{4,}$/;
+// minimum 4, at least one alphabets
+
+const schema = yup
+  .object({
+    email: string().required('Required').email('Cet email nest pas valide'),
+    password: string().required('Required').matches(passwordRules, {
+      message: 'Password should have min 4characters, more than 1 letter',
+    }),
+    confirmPassword: string()
+      .required('Required')
+      .oneOf([yup.ref('password'), null], 'Passwords must match'),
+    firstName: string().required('Required').matches(nameRules, {
+      message: 'Only alphabets are allowed.',
+    }),
+    lastName: string().required('Required').matches(nameRules, {
+      message: 'Only alphabets are allowed.',
+    }),
+  })
+  .required();
 
 const page = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const router = useRouter();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    router.push('/');
+    // redirect('/');
+    // actions after the redirection
+  };
+
   return (
     <main className="formMain">
       <div className="formContainer">
         <h1>Sign up</h1>
         <div className="formDiv">
-          <form className="form">
-            <label for="email">Email</label>
-            <input type="text" name="email" value="" />
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label>Email</label>
+              <input
+                {...register('email')}
+                placeholder="E-mail"
+                required
+                type="text"
+                name="email"
+              />
+            </div>
+            <div className="error">
+              {formState.errors.email?.message !== undefined
+                ? `${formState.errors.email?.message}`
+                : ''}
+            </div>
 
-            <label for="password">Password</label>
-            <input type="password" name="password" value="" />
+            <div>
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                {...register('password')}
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="error">
+              {formState.errors.password?.message !== undefined
+                ? `${formState.errors.password?.message}`
+                : ''}
+            </div>
 
-            <label for="firstName">First name</label>
-            <input type="firstName" name="text" value="" />
+            <div>
+              <label>Confirm password</label>
 
-            <label for="lastName">Last name</label>
-            <input type="lastName" name="text" value="" />
+              <input
+                className="form-input"
+                type="password"
+                placeholder="Confirm password"
+                {...register('confirmPassword')}
+              />
+            </div>
+            <div className="error">
+              {formState.errors.confirmPassword?.message !== undefined
+                ? `${formState.errors.confirmPassword?.message}`
+                : ''}
+            </div>
+
+            <div>
+              <label>First name</label>
+              <input
+                type="text"
+                name="firstName"
+                {...register('firstName')}
+                placeholder="First Name"
+                required
+              />
+            </div>
+            <div className="error">
+              {formState.errors.firstName?.message !== undefined
+                ? `${formState.errors.firstName?.message}`
+                : ''}
+            </div>
+
+            <div>
+              <label>Last name</label>
+              <input
+                type="text"
+                name="lastName"
+                {...register('lastName')}
+                placeholder="Last Name"
+                required
+              />
+            </div>
+            <div className="error">
+              {formState.errors.lastName?.message !== undefined
+                ? `${formState.errors.lastName?.message}`
+                : ''}
+            </div>
+            <button className="loginBtn">Submit</button>
           </form>
-          <div className={styles.buttonWrap}>
-            <button className="loginBtn" type="submit">
-              Submit
-            </button>
-          </div>
         </div>
       </div>
     </main>
