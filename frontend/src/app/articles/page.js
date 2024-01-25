e607e847ae8766bf0d6e177644d867d19dd0d3e2
Soeun Lee <thdms1005@gmail.com>
2024-01-25 19:10:48 +0100

    :sparkles: Add(logout btn) : add btn on articles page

--- frontend/src/app/articles/page.js
+++ frontend/src/app/articles/page.js
@@ -6,9 +6,18 @@ import DisplayArticle from '../components/DisplayArticle';
 import useGetArticle from '../hooks/useGetArticle';
 import styles from './articles.module.css';
 import Loading from '../components/Loading';
+import { useAuth } from '../hooks/useAuth';
+import { useRouter } from 'next/navigation';
 
 const page = () => {
   const { isLoading, articles } = useGetArticle();
+  const { removeToken } = useAuth();
+  const router = useRouter();
+
+  const handleOnClick = () => {
+    removeToken();
+    router.push('/');
+  };
 
   const token = localStorage.getItem('token');
 
@@ -16,12 +25,17 @@ const page = () => {
     if (!token) {
       redirect('/');
     }
-  }, []);
+  }, [token]);
 
   return (
     <div className={styles.container}>
       {isLoading && <Loading />}
-      <h1>articles</h1>
+      <div className={styles.titleWrap}>
+        <h1>articles</h1>
+        <button className={`${styles.btn} logoutBtn`} onClick={handleOnClick}>
+          Log out
+        </button>
+      </div>
       <div className={styles.articleWrap}>
         <DisplayArticle data={articles} />
       </div>
